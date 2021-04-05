4a9450b1489479b7f8d2a453c15b28f076eb808c
YilunAllenChen <allenchen@gatech.edu>
2021-04-05 15:16:08 -0400

    example added

--- example.py
+++ example.py
@@ -2,12 +2,38 @@ import findspark
 findspark.init()
 
 from pyspark import SparkContext
+from pyspark.sql.types import *
+from pyspark.sql import SQLContext
 
-sc = SparkContext("local", "stock analysis")
+sc = SparkContext("local", "Stock Analysis - ECE4150")
+sqlContext = SQLContext(sc)
 
-lines = sc.textFile("./datasets/Stocks/aapl.us.txt")
+txt = sc.textFile("./datasets/Stocks/aapl.us.txt")
+header = txt.first()
+schemaString = header.replace('"','')
+newschema = [StructField(field_name, StringType(), True) for field_name in schemaString.split(',')][:-1]
 
-llist = lines.collect()
+for i in range(1, len(newschema)):
+    newschema[i].dataType = FloatType()
 
-for line in llist:
-    print(line)
+schema = StructType(newschema)
+
+header_ref = txt.filter(lambda l: "Open" in l)
+header_ref.collect()
+txt = txt.subtract(header_ref)
+txt_processed = txt.map(lambda l: l.split(",")).map(lambda d: (d[0], float(d[1]), float(d[2]), float(d[3]), float(d[4]), float(d[5])))
+
+df = sqlContext.createDataFrame(txt_processed, schema)
+
+
+all_days = df.count()
+up_days = df.filter(df.Open < df.Close).count()
+flat_days = df.filter(df.Open == df.Close).count()
+down_days = df.filter(df.Open > df.Close).count()
+
+
+
+print(all_days)
+print(up_days)
+print(flat_days)
+print(down_days)
