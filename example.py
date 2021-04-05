66712ad0605ce12cdf7419342f2380305aa1a049
YilunAllenChen <allenchen@gatech.edu>
2021-04-05 16:31:53 -0400

    example updated; output added;

--- example.py
+++ example.py
@@ -5,35 +5,33 @@ from pyspark import SparkContext
 from pyspark.sql.types import *
 from pyspark.sql import SQLContext
 
+import os
+
+
 sc = SparkContext("local", "Stock Analysis - ECE4150")
 sqlContext = SQLContext(sc)
 
-txt = sc.textFile("./datasets/Stocks/aapl.us.txt")
-header = txt.first()
-schemaString = header.replace('"','')
-newschema = [StructField(field_name, StringType(), True) for field_name in schemaString.split(',')][:-1]
-
-for i in range(1, len(newschema)):
-    newschema[i].dataType = FloatType()
-
-schema = StructType(newschema)
-
-header_ref = txt.filter(lambda l: "Open" in l)
-header_ref.collect()
-txt = txt.subtract(header_ref)
-txt_processed = txt.map(lambda l: l.split(",")).map(lambda d: (d[0], float(d[1]), float(d[2]), float(d[3]), float(d[4]), float(d[5])))
-
-df = sqlContext.createDataFrame(txt_processed, schema)
-
-
-all_days = df.count()
-up_days = df.filter(df.Open < df.Close).count()
-flat_days = df.filter(df.Open == df.Close).count()
-down_days = df.filter(df.Open > df.Close).count()
-
-
-
-print(all_days)
-print(up_days)
-print(flat_days)
-print(down_days)
+with open("output.txt", "w+") as f:
+    dataset_location = './datasets/Stocks'
+    stocks = os.listdir(dataset_location)
+    f.write("Stock, All Days, Up Days, Flat Days, Down Days")
+    for stock in stocks:
+        txt = sc.textFile(f"{dataset_location}/{stock}")
+        if txt.isEmpty():
+            continue
+        header = txt.first()
+        schemaString = header.replace('"','')
+        newschema = [StructField(field_name, StringType(), True) for field_name in schemaString.split(',')][:-1]
+        for i in range(1, len(newschema)):
+            newschema[i].dataType = FloatType()
+        schema = StructType(newschema)
+        header_ref = txt.filter(lambda l: "Open" in l)
+        header_ref.collect()
+        txt = txt.subtract(header_ref)
+        txt_processed = txt.map(lambda l: l.split(",")).map(lambda d: (d[0], float(d[1]), float(d[2]), float(d[3]), float(d[4]), float(d[5])))
+        df = sqlContext.createDataFrame(txt_processed, schema)
+        all_days = df.count()
+        up_days = df.filter(df.Open < df.Close).count()
+        flat_days = df.filter(df.Open == df.Close).count()
+        down_days = df.filter(df.Open > df.Close).count()
+        f.write(f"{stock}, {all_days}, {up_days}, {down_days}, {down_days}\n")
